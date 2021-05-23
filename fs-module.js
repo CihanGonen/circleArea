4a9576f5c8bb2a957e28ea1e2f03e1db6b0a8821
Cihan <cihangonen35@gmail.com>
2021-05-23 23:28:33 +0300

    fs practice

---
+++ fs-module.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+// read file
+
+// fs.readFile('fsPractice.txt','utf8',(err,data)=>{
+//   if(err) console.log(err);
+//   console.log(data);
+//   console.log('dosya başarıyla okundu')
+// });
+
+// fs.writeFile('fsWrite.txt','Cihan Gönen','utf8',(err,data)=>{
+//   if(err) console.log(err);
+//   console.log('dosya başarıyla oluşturuldu')
+// })
+
+// fs.appendFile('fsWrite.txt','\n KODLUYOR','utf8',(err,data)=>{
+//   if(err) console.log(err);
+//   console.log('dosyaya başarıyla ekleme yapıldı')
+// })
+
+// fs.unlink('fsPractice.txt',(err)=>console.log(err))
+
+// fs.mkdir('uploads/img',{recursive:true},(err)=>{
+//   if(err) console.log(err);
+//   console.log('klasör oluşturuldu');
+// })
+
+fs.rmdir('uploads',{recursive:true},(err)=>{
+  if(err) console.log(err);
+  console.log('klasör silindi');
+})
